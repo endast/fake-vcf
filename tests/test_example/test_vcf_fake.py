f86f37e403d45fd2c3ad6c1446d48b2164e1cc6d
Magnus Wahlberg <endast@gmail.com>
2023-07-28 15:21:14 +0200

    Add tests for generation with data

--- tests/test_example/test_vcf_fake.py
+++ tests/test_example/test_vcf_fake.py
@@ -1,14 +1,25 @@
 """Tests for fake_vcf"""
+import typing
+
 import pytest
 
 from fake_vcf.vcf_faker import VirtualVCF
-from fake_vcf.vcf_generator import fake_vcf
+
+
+def get_vcf_data(
+    virtual_vcf: VirtualVCF,
+) -> typing.Tuple[typing.List[str], typing.List[str]]:
+    with virtual_vcf as v_vcf:
+        vcf_rows = list(v_vcf)
+        data_rows = [r for r in vcf_rows if not r.startswith("##")]
+        metadata = vcf_rows[0]
+    return data_rows, metadata
 
 
 @pytest.mark.parametrize(
     ("num_rows",),
     [
-        *[(r,) for r in range(0, 11)],
+        *[(r,) for r in range(1, 11)],
         (20,),
         (50,),
         (1000,),
@@ -25,7 +36,31 @@ def test_fake_vcf_row_count(num_rows):
         phased=False,
     )
 
-    with virtual_vcf as v_vcf:
-        vcf_rows = list(v_vcf)
-        data_rows = [r for r in vcf_rows if not r.startswith("##")]
-        assert len(data_rows) == num_rows
+    data_rows, metadata = get_vcf_data(virtual_vcf=virtual_vcf)
+    assert len(data_rows) == num_rows
+
+
+@pytest.mark.parametrize(
+    ("num_samples",),
+    [
+        *[(r,) for r in range(1, 11)],
+        (20,),
+        (50,),
+        (1000,),
+        (1337,),
+    ],
+)
+def test_fake_vcf_sample_count(num_samples):
+    virtual_vcf = VirtualVCF(
+        num_rows=10,
+        num_samples=num_samples,
+        chromosome="chr1",
+        sample_prefix="S",
+        random_seed=42,
+        phased=False,
+    )
+
+    nr_non_sample_col = 9
+    data_rows, metadata = get_vcf_data(virtual_vcf=virtual_vcf)
+    samples = data_rows[0].split("\t")
+    assert len(samples) - nr_non_sample_col == num_samples
