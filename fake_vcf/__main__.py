3b7ab8afdaf3c1e4724d0bdca9fbcf8fb95a063e
Magnus Wahlberg <endast@gmail.com>
2023-07-28 14:03:13 +0200

    Make cli work

--- fake_vcf/__main__.py
+++ fake_vcf/__main__.py
@@ -7,16 +7,6 @@ from rich.console import Console
 from fake_vcf import version
 from fake_vcf.vcf_generator import fake_vcf
 
-
-class Color(str, Enum):
-    white = "white"
-    red = "red"
-    cyan = "cyan"
-    magenta = "magenta"
-    yellow = "yellow"
-    green = "green"
-
-
 app = typer.Typer(
     name="fake-vcf",
     help="A fake vcf file generator",
@@ -32,39 +22,30 @@ def version_callback(print_version: bool) -> None:
         raise typer.Exit()
 
 
-"""
-
-@click.command()
-@click.option(
-    "--fake_vcf_path",
-    "-o",
-    default=None,
-    type=click.Path(exists=False, writable=True, path_type=Path),
-)
-@click.option("--num_rows", default=10, help="Nr rows to generate (variants)")
-@click.option("--num_samples", default=10, help="Nr of num_samples to generate.")
-@click.option("--chromosome", default="chr1", help="chromosome default chr1")
-@click.option("--seed", default=42, help="Random seed to use")
-@click.option(
-    "--sample_prefix",
-    default="S",
-    help="Sample prefix ex: SAM =>  SAM0000001	SAM0000002",
-)
-@click.option("--phased/--no-phased", default=True, help="Simulate phased")
-
-"""
-
-
 @app.command(name="")
 def main(
     fake_vcf_path: Path = typer.Option(
         None,
         "--fake_vcf_path",
         "-o",
-        case_sensitive=False,
         help="Path to fake vcf file.",
     ),
-    num_rows: int = typer.Option(),
+    num_rows: int = typer.Option(
+        10, "--num_rows", help="Nr rows to generate (variants)"
+    ),
+    num_samples: int = typer.Option(
+        10, "--num_samples", help="Nr of num_samples to generate."
+    ),
+    chromosome: str = typer.Option(
+        "chr1", "--chromosome", help="chromosome default chr1"
+    ),
+    seed: int = typer.Option(42, "--seed", help="Random seed to use"),
+    sample_prefix: str = typer.Option(
+        "S",
+        "--sample_prefix",
+        help="Sample prefix ex: SAM =>  SAM0000001	SAM0000002",
+    ),
+    phased: bool = typer.Option(default=True, help="Simulate phased"),
     print_version: bool = typer.Option(
         None,
         "-v",
@@ -76,12 +57,12 @@ def main(
 ) -> None:
     fake_vcf(
         fake_vcf_path=fake_vcf_path,
-        num_rows=None,
-        num_samples=None,
-        chromosome=None,
-        seed=None,
-        sample_prefix=None,
-        phased=None,
+        num_rows=num_rows,
+        num_samples=num_samples,
+        chromosome=chromosome,
+        seed=seed,
+        sample_prefix=sample_prefix,
+        phased=phased,
     )
 
 
