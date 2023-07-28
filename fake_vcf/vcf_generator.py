0a063b3b5acd36e70833794e27e6029a0cc36b7a
Magnus Wahlberg <endast@gmail.com>
2023-07-28 12:33:43 +0200

    Remove hello

--- fake_vcf/vcf_generator.py
+++ fake_vcf/vcf_generator.py
@@ -1,19 +1,42 @@
-"""Example of code."""
+import gzip
+import sys
+
+import tqdm
+
+from fake_vcf.vcf_faker import VirtualVCF
 
 
-def hello(name: str) -> str:
-    """Just a greetings example.
+def fake_vcf(
+    fake_vcf_path, num_rows, num_samples, chromosome, seed, sample_prefix, phased
+):
+    virtual_vcf = VirtualVCF(
+        num_rows=num_rows,
+        num_samples=num_samples,
+        chromosome=chromosome,
+        sample_prefix=sample_prefix,
+        random_seed=seed,
+        phased=phased,
+    )
 
-    Args:
-        name (str): Name to greet.
+    if fake_vcf_path is None:
+        with virtual_vcf as v_vcf:
+            for line in v_vcf:
+                sys.stdout.write(line)
+        return
 
-    Returns:
-        str: greeting message
+    print(f"Writing to file {fake_vcf_path}")
 
-    Examples:
-        .. code:: python
+    if fake_vcf_path.suffix == ".gz":
+        print("(Using compression)")
+        with gzip.open(fake_vcf_path, "wt") as gz_file, virtual_vcf as v_vcf:
+            for line in tqdm.tqdm(v_vcf, total=num_rows + 1):
+                gz_file.write(line)
+    else:
+        print("(No compression)")
+        with open(
+            fake_vcf_path, "w", encoding="utf-8"
+        ) as txt_file, virtual_vcf as v_vcf:
+            for line in tqdm.tqdm(v_vcf, total=num_rows + 1):
+                txt_file.write(line)
 
-            >>> hello("Roman")
-            'Hello Roman!'
-    """
-    return f"Hello {name}!"
+    print(f"Done, data written to {fake_vcf_path}")
