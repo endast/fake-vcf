7f4586a5899e5ec8d396d4920e55f74753ecf02b
Magnus Wahlberg <endast@gmail.com>
2024-02-27 13:34:50 +0100

    add test_parse_sequences_content_sum

--- tests/test_vcf_fake_reference.py
+++ tests/test_vcf_fake_reference.py
@@ -26,10 +26,30 @@ test_data_dir = Path(__file__).resolve().parent / "test_data"
     ),
 )
 def test_parse_sequences(include_sequences, fasta_file):
-    include_sequences = [f"chr{c}" for c in range(1, 11)]
     fasta_file = test_data_dir / "reference/reference_small.fa"
     sequences = refrence.parse_fasta(
         include_sequences=include_sequences, file_path=fasta_file
     )
+
+    include_sequences = [] if include_sequences is None else include_sequences
+
     assert len(sequences) == len(include_sequences)
     assert all(seq["id"] in include_sequences for seq in sequences)
+    assert all(len(seq["sequence"]) > 0 for seq in sequences)
+    assert all(seq["sequence"] for seq in sequences)
+
+
+@pytest.mark.parametrize(
+    "sequence_id, fasta_file, expected_sequence_sum",
+    (
+        (f"chr1", test_data_dir / "reference/reference_small.fa", 77573),
+        (f"chr2", test_data_dir / "reference/reference_small.fa", 95119),
+        (f"chr5", test_data_dir / "reference/reference_small.fa", 129915),
+    ),
+)
+def test_parse_sequences_content_sum(sequence_id, fasta_file, expected_sequence_sum):
+    fasta_file = test_data_dir / "reference/reference_small.fa"
+    sequences = refrence.parse_fasta(
+        include_sequences=[sequence_id], file_path=fasta_file
+    )
+    assert sum([ord(s) for s in sequences[0]["sequence"]]) == expected_sequence_sum
